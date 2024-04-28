df2b9200ee432b98ee2d3a2a98321cbfa6e1b2a0
siskoweb <sisko4dev@gmail.com>
2024-04-28 17:56:41 +0100

    display color for each region with extaned of how many pateints they have now i will add div wher display number of patinets

--- frontend/src/components/charts/cities/MapChart.tsx
+++ frontend/src/components/charts/cities/MapChart.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useState, type FC } from "react";
+import { Key, useMemo, useRef, useState, type FC } from "react";
 import {
   MapContainer,
   Marker,
@@ -22,7 +22,6 @@ const MapChart: FC<MapChartProps> = ({}) => {
   const center: Number[] = [28.996643486255092, -9.707703872193896];
 
   const [onselect, setOnselect] = useState({});
-  const [activeCity, setActiveCity] = useState(false);
 
 
 
@@ -30,17 +29,21 @@ const MapChart: FC<MapChartProps> = ({}) => {
   /* function determining what should happen onmouseover, this function updates our state*/
   const highlightFeature = (e: any) => {
     var layer = e.target;
-    const { cartodb_id, region } = e.target.feature.properties;
+    layer.setStyle(styleHover());
+      const result = dataPatient.filter(
+        (item) => item.region === e.target.feature.properties.name
+      );
+
+
     setOnselect({
-      region: region,
-      cartodb_id: cartodb_id,
+      patients: result.length > 0 ? result[0].number_patients : 0,
     });
-    layer.setStyle({
-      weight: 2,
-      color: "white",
-      fillOpacity: 1,
+    // layer.setStyle({
+    //   weight: 2,
+    //   color: "white",
+    //   fillOpacity: 1,
       
-    });
+    // });
   };
 
 
@@ -65,16 +68,15 @@ const MapChart: FC<MapChartProps> = ({}) => {
 
 
   const whenClick = (e: any) => {
-    e.target.setStyle(styleHover(e.target.feature));
+    e.target.setStyle(styleHover());
     const result = dataPatient.filter(
       (item) => item.region == e.target.feature.properties.name
     );
 
-  
+      setOnselect({
+        patients: result.length > 0 ? result[0].number_patients : 0,
+      });
 
-
-
-    console.log(result);
  
   };
 
@@ -82,49 +84,65 @@ const MapChart: FC<MapChartProps> = ({}) => {
   // this function is called when a feature in the map is hovered over or when a mouse moves out of it, the function calls two functions
   const onEachFeature = (feature: any, layer: any) => {
     layer.on({
-      // mouseover: highlightFeature,
+      mouseover: highlightFeature,
       mouseout: resetHighlight,
-      click: whenClick,
+   
     });
   };
 
+  //  click: whenClick,
 
 
-  const mapPolygonColorToDensity = (cartodb_id: number) => {
-    return cartodb_id == 1
-      ? "#a50f15"
-      : cartodb_id > 3
-      ? "#de2d26"
-      : cartodb_id > 5
-      ? "#fb6a4a"
-      : cartodb_id > 6
-      ? "#fc9272"
-      : cartodb_id > 7
-      ? "#fcbba1"
-      : "#fc9272";
-  };
+  //  add spicefec color depand of  number of patients in region
+const getColorForRegion = (properties: any) => {
+  const result = dataPatient.filter((item) => item.region === properties.name);
+
+  const number_patients = result.length > 0 ? result[0].number_patients : 0;
+
+  return number_patients > 150
+    ? "#800026"
+    : number_patients > 120
+    ? "#BD0026"
+    : number_patients > 100
+    ? "#E31A1C"
+    : number_patients > 50
+    ? "#FC4E2A"
+    : number_patients > 20
+    ? "#FD8D3C"
+    : number_patients > 10
+    ? "#FEB24C"
+    : number_patients >= 1
+    ? "#FED976"
+    : "#FFEDA0";
+};
+
+
+
+
 
 
   // style for each part from map
   const style = (feature: any) => {
     return {
-      fillColor: mapPolygonColorToDensity(feature.properties.cartodb_id),
+      fillColor: getColorForRegion(feature.properties),
       weight: 1,
       opacity: 1,
       color: "white",
-      dashArray: "",
+      dashArray: "3",
       fillOpacity: 0.5,
     };
   };
 
 
-  const styleHover = (feature: any) => {
+
+
+  const styleHover = () => {
     return {
       fillColor: 'black',
       weight: 1,
       opacity: 1,
       color: "white",
-      dashArray: "",
+      dashArray: "3",
       fillOpacity: 0.5,
     };
   };
@@ -136,15 +154,16 @@ const MapChart: FC<MapChartProps> = ({}) => {
     width: "100%",
     margin: "0 auto",
     
-    
-
   };
+
+
 
   const customMarkerIcon = (name: string) =>
     divIcon({
       html: name,
       className: "icon",
     });
+
 
   // display name city
   const setIcon = ({ properties }: any, latlng: any) => {
@@ -154,15 +173,17 @@ const MapChart: FC<MapChartProps> = ({}) => {
 
 
 
-//   const feature = statesData.features.map((feature) => {
-// ;
-//     return feature;
-//   });
 
-
+const icon = L.icon({
+  iconSize: [25, 41],
+  iconAnchor: [10, 41],
+  popupAnchor: [2, -40],
+  iconUrl: "https://unpkg.com/leaflet@1.6/dist/images/marker-icon.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.6/dist/images/marker-shadow.png",
+});
 
   return (
-    <div className="z-10 flex justify-center flex-col lg:w-1/2  w-full   h-auto bg-white rounded-md my-4 p-4">
+    <div className="z-10 flex justify-center flex-col lg:w-1/2   h-auto bg-white rounded-md my-4 p-4">
       <MapContainer
         attributionControl={false}
         center={center as any}
@@ -177,49 +198,50 @@ const MapChart: FC<MapChartProps> = ({}) => {
         boxZoom={false}
         keyboard={false}
       >
+        <div
+          style={{ position: "absolute", zIndex: 800 }}
+          className=" bg-white rounded-md shadow-sm opacity-60  w-2/6 px-2 pt-4  min-h-20 flex flex-col gap-3 items-start"
+        >
+          <p className="text-xs font-semibold">Nombre de patients par entité</p>
+          {onselect ? (
+            <p>{onselect && onselect?.patients}</p>
+          ) : (
+            <p>click over a region</p>
+          )}
+        </div>
+
+        {statesData.features.map(
+          (
+            city: { geometry: { coordinates: any[][] } },
+            index: Key | null | undefined
+          ) => {
+            const coordinates = city.geometry.coordinates[0].map(
+              (item: any[]) => [item[0], item[1]]
+            );
+
+            return (
+              <GeoJSON
+                key={index}
+                data={statesData.features as any}
+                style={style}
+                onEachFeature={onEachFeature}
+                pointToLayer={setIcon}
+
+                // pathOptions={style as any}
+              />
+            );
+          }
+        )}
+
         <TileLayer
           attribution="yassine"
           // url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           url="https://{s}.basemaps.cartocdn.com/rastertiles/light_nolabels/{z}/{x}/{y}.png"
         />
-
-
-
-        {/* {cities.map((city, index) => {
-          return (
-            <Marker
-              key={index}
-              position={[Number(city.lng), Number(city.lat)]}
-              // onClick={() => {
-              //   return setActiveCity(city);
-              // }}
-            />
-          
-          
-          );
-        })} */}
-
-
-        {statesData.features.map((city, index) => {
-          const coordinates = city.geometry.coordinates[0].map((item) => [
-            item[0],
-            item[1],
-          ]);
-
-          return (
-            <GeoJSON
-              key={index}
-              data={statesData.features as any}
-              style={style}
-              onEachFeature={onEachFeature}
-              pointToLayer={setIcon}
-              // pathOptions={style as any}
-            />
-          );
-        })}
-        <div className="z-50">Hola</div>
       </MapContainer>
     </div>
   );
 };
 export default MapChart;
+
+
