a861fc8e1a107a23a035616aeaa171c8c44b8a42
siskoweb <sisko4dev@gmail.com>
2024-04-29 13:57:42 +0100

    desbugging not found page frontend

--- frontend/src/app/[locale]/page.tsx
+++ frontend/src/app/[locale]/page.tsx
@@ -29,19 +29,19 @@ const correntlocale = useLocale();
                 className="h-16 w-auto"
               />
             </a>
- 
+
             <div className="flex ml-auto">
               {session?.token ? (
                 <Link
                   href={"/dashboard"}
-                  className="px-6 py-3 rounded-xl text-white bg-cyan-900 transition-all hover:bg-cyan-800"
+                  className="px-6 py-3 rounded-xl text-white bg-theme-color transition-all hover:opacity-85 duration-150"
                 >
                   Dashboard
                 </Link>
               ) : (
                 <Link
                   href={"/login"}
-                  className="px-6 py-3 rounded-xl text-white bg-cyan-900 transition-all hover:bg-cyan-800"
+                  className="px-6 py-3 rounded-xl text-white bg-theme-color transition-all hover:opacity-85 duration-150"
                 >
                   Login
                 </Link>
@@ -49,7 +49,7 @@ const correntlocale = useLocale();
             </div>
           </div>
         </header>
-        <div className="relative">
+        <div className="relative ">
           <div className="px-4 sm:px-10 ">
             <div className="mt-16 max-w-4xl mx-auto text-center relative z-10">
               <h1 className="md:text-6xl text-4xl font-extrabold mb-6 md:!leading-[75px]">
@@ -61,23 +61,27 @@ const correntlocale = useLocale();
                 experience today. Lorem ipsum dolor sit amet, consectetur
                 adipiscing elit.
               </p>
-              <div className="mt-10">
-                <button className="px-6 py-3 rounded-xl text-white bg-cyan-900 transition-all hover:bg-cyan-800">
+              {/* <div className="mt-10">
+                <button className="px-6 py-3 rounded-xl text-white bg-theme-color transition-all hover:opacity-85 duration-150">
                   Get started today
                 </button>
-              </div>
+              </div> */}
             </div>
-            <hr className="my-12 border-gray-300" />
+            {/* <hr className="my-12 border-gray-300" /> */}
 
-            <img
+            <Image
               src="/bgContent1.png"
               className="absolute right-0 top-0 w-auto h-auto bg-repeat"
-              alt="pinterest-logo"
+              alt="logo"
+              width="20"
+              height="20"
             />
-            <img
+            <Image
               src="/bgContent1.png"
               className="absolute inset-0 w-auto h-auto bg-repeat"
-              alt="pinterest-logo"
+              alt="logo"
+              width="20"
+              height="20"
             />
           </div>
         </div>
--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Error from "next/error";
+import Link from "next/link";
 
 // Render the default Next.js 404 page when a route
 // is requested that doesn't match the middleware and
@@ -11,6 +12,7 @@ export default function NotFound() {
     <html lang="en">
       <body>
         <Error statusCode={404} />
+        <Link href="/">Return Home</Link>
       </body>
     </html>
   );
